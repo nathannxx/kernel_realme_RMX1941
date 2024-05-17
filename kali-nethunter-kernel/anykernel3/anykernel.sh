29cef32fd115e6fac55363d920c3d654dcb2d6fb
Frostleaft07 <zx7unknow@gmail.com>
2024-05-17 06:48:41 +0000

    anykernel3: update anykernel.sh and banner

--- kali-nethunter-kernel/anykernel3/anykernel.sh
+++ kali-nethunter-kernel/anykernel3/anykernel.sh
@@ -6,33 +6,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetHunter Kernel for the OnePlus 7 Pro
+kernel.string=NetHunter Kernel
 do.devicecheck=1
 do.modules=1
 do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus7Pro
-device.name2=guacamole
-device.name3=OnePlus 7 Pro
-device.name4=Guacamole
-device.name5=OnePlus7
-device.name6=guacamoleb
-device.name7=OnePlus 7
-device.name8=Guacamoleb
-device.name9=OnePlus7ProNR
-device.name10=OnePlus7ProTMO
-device.name11=hotdogb
-device.name12=hotdog
-device.name13=OnePlus7T
-device.name14=OnePlus7TPro
+device.name1=RMX2180
+device.name2=RMX2185
+device.name3=Nightmare
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+block=/dev/block/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -54,23 +43,11 @@ dump_boot;
 # begin ramdisk changes
 
 # init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# backup_file init.rc;
+# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
 write_boot;
 ## end install
-
